41a51ecbe5f015e7f4d2a67b5010b9c6d25801ab
Rahul Patil <rahuldpatil20@gmail.com>
2025-07-22 16:39:44 -0700

    Initial Commit

---
+++ Sentio-Mobile/Sentio_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentio_MobileApp.swift
+//  Sentio-Mobile
+//
+//  Created by Rahul Patil on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Sentio_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
